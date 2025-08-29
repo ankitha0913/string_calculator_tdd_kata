6b7fc07d674b70fd57d305f4c3a3b1b4db7a9f10
Ankitha <ankitha@robosoftin.com>
2025-08-29 13:29:40 +0530

    comma separated cases

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
 void main(){
+   print(add("0"));
+}
 
+int add(String numbers){
+  if(numbers.isEmpty) {
+    return 0;
+  }
+  return numbers.split(",").map(int.parse).reduce((a, b) => a + b);
 }
--- test/main_test.dart
+++ test/main_test.dart
@@ -1,3 +1,22 @@
-void main() {
+import 'package:string_calculator_tdd_kata/main.dart';
+import 'package:test/test.dart';
 
+void main() {
+  group('add', () {
+    test('returns 0 when the string is empty', () {
+      expect(add(''), equals(0));
+    });
+
+    test('returns the number when there is only one number', () {
+      expect(add('5'), equals(5));
+    });
+
+    test('returns the sum of two numbers', () {
+      expect(add('1,2'), equals(3));
+    });
+
+    test('returns the sum of multiple numbers', () {
+      expect(add('1,2,3,4'), equals(10));
+    });
+  });
 }
